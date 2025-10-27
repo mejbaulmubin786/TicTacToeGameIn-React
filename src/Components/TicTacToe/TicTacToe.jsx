574d8201a21f41f4a68faf5622fe1805e74e50b0
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-27 21:28:01 +0600

    css update

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './TicTacToe.css'
+import circle_icon from '../assets/circle.png'
+import cross_icon from '../assets/cross.png'
 
 const TicTacToe = () => {
   return (
-    <div>
-        Tic
+    <div className='container'>
+        <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
+        <div className="board"></div>
+        <button className='reset'>Reset</button>
     </div>
   )
 }
