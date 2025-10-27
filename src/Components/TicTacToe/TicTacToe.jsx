b06b7be7ab8dfe9df28b73cf0b5090885fb94421
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-10-27 22:00:10 +0600

    main styles is ready

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -7,7 +7,23 @@ const TicTacToe = () => {
   return (
     <div className='container'>
         <h1 className="title">Tic Tac Toe Game In <span>React</span></h1>
-        <div className="board"></div>
+        <div className="board">
+          <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+          <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+          <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+        </div>
         <button className='reset'>Reset</button>
     </div>
   )
